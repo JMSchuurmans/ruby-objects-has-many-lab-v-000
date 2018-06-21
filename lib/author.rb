55c96a3a84d43247dfb302da28e6856b26223ada
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-06-21 15:50:28 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -11,8 +11,8 @@ class Author
     post.author = self
   end
 
-  def add_post_by_title
-    post = Post.new
+  def add_post_by_title(title)
+    post = Post.new(title)
     @posts << post
     post.author = self
   end
